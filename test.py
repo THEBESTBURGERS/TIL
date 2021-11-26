9c87784506ec458b0e85a6c18b1ff20ef90014e7
chestnut <wbk1620@gmail.com>
2021-11-26 23:15:49 +0900

    .

--- test.py
+++ test.py
@@ -2,18 +2,13 @@ T = int(input())
 
 for i in range(T):
     A, Z = int(input().split())
-    test = Z-A
-    test_child = 1
-    result = 0
-    result_try = 0
-    while True:
-        if test == 1:
-            result_try = 1
-            break
-        elif result < test:
-            result = result + test_child*2
-            result_try += 1
-            if test - result > result_try:
-                hh = 0
-
-    print(result_try)
+    distance = Z-A
+    cnt = 1
+    gap = 1
+    for i in range(1, distance+1):
+        if (i == distance):
+            print("끝")
+            print(cnt)
+        else:
+            for j in range(1, distance):
+                cnt = cnt + j
