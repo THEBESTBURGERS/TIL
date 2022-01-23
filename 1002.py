aa793120a67ce3f095dd57ceb8fa3ece91477cb9
chestnut1044 <wbk1620@gmail.com>
2022-01-23 23:15:47 +0900

    1002

--- 1002.py
+++ 1002.py
@@ -4,6 +4,13 @@ T = int(input())
 T_list = []
 for i in range(T):
     t = list(map(int, input().split()))
+
+
+
+# 내접일때
+# 만나지 않을때
+# 만날때 
+
     d = math.sqrt((t[3]-t[0])*(t[3]-t[0])+(t[4]-t[1])*(t[4]-t[1]))
     r = t[2] + t[5]
     if(t[0]==t[3] and t[1]==t[4] and t[2]==t[5]):
