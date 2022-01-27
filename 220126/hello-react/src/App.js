d3e62b124fc46e131c3fc40fed3f7de5d9a86790
chestnut1044 <wbk1620@gmail.com>
2022-01-27 17:38:11 +0900

    컴포넌트 학습

--- 220126/hello-react/src/App.js
+++ 220126/hello-react/src/App.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react'
-import './App.css'
+import MyComponent from './MyComponent';
+
 class App extends Component {
   render() {
-    const text = "당신은 어썸한가요?";
-    const condition = true;
-    const style = {
-      backgroundColor: 'brown',
-      border: '1px solid black',
-      height: Math.round(Math.random()*300) + 50,
-      width: Math.round(Math.random()*300) + 50,
-      WebkitTransition: 'all',
-      MozTransition: 'all',
-      msTransition: 'all'
-    };
-      return (
-        <div className="my-div">
-          {/* 요소 밖에서는 이렇게 작성해요.*/}
-          <h1>리액트 안녕</h1>
-          <h2>{text}</h2>
-          { condition && '보여주세요' }
-          <div style={style}
-            // self-closed 태그에서만 작동하는 주석
-            // 마지막 />가 꼭 새 줄에 있어야 합니다.
-            /* 이렇게 작성해도 됩니다. */
-          />
-          // 여기에 쓰는건 그대로 렌더링 됩니다.
-          /* 여기에선 주석 못 써요 */
-        </div>
-      )
+    return (
+      <MyComponent/>
+    );
   }
 }
+
 export default App;
-
-
-
