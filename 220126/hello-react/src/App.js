4248b6ed86bd9585f4411ea264218aea0af06860
chestnut1044 <wbk1620@gmail.com>
2022-01-27 15:42:05 +0900

    inline style

--- 220126/hello-react/src/App.js
+++ 220126/hello-react/src/App.js
@@ -4,14 +4,21 @@ class App extends Component {
   render() {
     const text = "당신은 어썸한가요?";
     const condition = true;
-
+    const style = {
+      backgroundColor: 'brown',
+      border: '1px solid black',
+      height: Math.round(Math.random()*300) + 50,
+      width: Math.round(Math.random()*300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    };
       return (
         <>
           <h1>리액트 안녕</h1>
           <h2>{text}</h2>
-          {
-            condition ? '참' : '거짓'
-          }
+          { condition && '보여주세요' }
+          <div style={style}></div>
         </>
       )
   }
