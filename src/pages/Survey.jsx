476e8b6ee17f5708899e99e933c3976124b0c470
chestnut1044 <wbk1620@gmail.com>
2022-01-31 02:43:56 +0900

    make progressbar

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,13 +1,40 @@
-import { ProgressBar }  from '../styles/Survey'
+import { Bar_background, Bar_content, Survey_text, Content, Checkbox, Text}  from '../styles/Survey'
+import { useState ,useEffect } from 'react'
+
+const num = 10
+
 
 const Survey = () => {
-    return (
-      <>
-        <ProgressBar>
-          프로그래스바
-        </ProgressBar>
-      </>
-    );
-  }
+    const [count, setCount] = useState(0);
+    function add_count(){
+      if(count == num){
+        setCount(0);
+      }
+      else{
+        setCount(count + 1);
+      }
+    }
   
-  export default Survey;
+
+  return (
+    <>
+      <Survey_text>
+        Survey
+      </Survey_text>
+      <Content>
+        <Bar_background onClick={()=>{add_count()}}>
+          <Bar_content width={(count/num)*100 + "%"}>
+          </Bar_content>
+        </Bar_background>
+        <Text>
+          원하는 개발 분야를 선택해주세요
+        </Text>
+        <Checkbox>
+          테스트
+        </Checkbox>
+      </Content>
+    </>
+  );
+}
+
+export default Survey;
--- src/styles/Survey.js
+++ src/styles/Survey.js
@@ -1,6 +1,54 @@
 import styled from 'styled-components';
-import React  from "react";
 
-export const ProgressBar = styled.div`
-    display: flex;
+export const Survey_text = styled.div`
+	display: flex;
+	font-family: var(--font-body);
+	font-size: 1.7rem;
+	font-weight: bold;
+	color: white;
+	padding : 2rem;
+	padding-bottom:3rem;
+`;
+
+
+export const Content = styled.section`
+	display: flex;
+	flex-direction:column;
+	align-content: center;
+	align-items: center;
+`;
+
+
+export const Bar_background = styled.div`
+	background-color: white;
+	width: 50em;
+	height: 1.2em;
+	align-items: center;
+	border-radius: 20px;
+`;
+
+export const Bar_content = styled.div`
+	background-color: var(--color-orange);
+	width: ${props => props.width};
+	height: 1.2em;
+	align-items: center;
+	border-radius: 20px;
+	transition-delay: 0.5s;
+	transition: 1s ease;
+`;
+
+
+export const Text = styled.h1`
+	font-family: var(--font-body);
+	font-size: 1.7rem;
+	color: white;
+	padding:1.5em;
+	margin:0;
+`;
+
+
+
+export const Checkbox = styled.div`
+	color: white;
+	padding : 1rem;
 `;
