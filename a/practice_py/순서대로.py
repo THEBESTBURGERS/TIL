d6a871e0e36c911bc4e737b2923c55a63a2c0160
chestnut1044 <wbk1620@gmail.com>
2023-08-30 03:53:34 +0900

    test

--- a/practice_py/순서대로.py
+++
@@ -1,8 +0,0 @@
-t = int(input())
-arr = []
-for i in range(0, t):
-    age, name = input().split()
-    arr.append((age, name))
-arr.sort(key=lambda x: x[0])
-for i in range(0, t):
-    print(arr[t-1-i][0], arr[t-i-1][1])
